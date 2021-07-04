e8992c3b2809d1b6e9528b63f78f14885f8924b7
sipakov <sipakov1987@gmail.com>
2021-07-04 22:45:18 +0300

    updated attributes in interface

--- data-serialization/Interfaceses/IXmlToJsonConverter.cs
+++ data-serialization/Interfaceses/IXmlToJsonConverter.cs
@@ -1,9 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace data_serialization.Interfaceses
 {
     public interface IXmlToJsonConverter
     {
-        Task<bool> Convert(string pathToTargetXml, string pathToOutputJson);
+        Task<bool> Convert([NotNull]string pathToTargetXml, [NotNull]string pathToOutputJson);
     }
 }
